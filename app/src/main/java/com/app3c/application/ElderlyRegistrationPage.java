0212f475d107d6e8d97c8c7c29b77ea7e106b047
Arya R N <aryakunjamma@gmail.com>
2022-10-01 23:57:02 +0530

    Added code for connecting to database

--- app/src/main/java/com/app3c/application/ElderlyRegistrationPage.java
+++ app/src/main/java/com/app3c/application/ElderlyRegistrationPage.java
@@ -1,19 +1,105 @@
 package com.app3c.application;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ElderlyRegistrationPage extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://application-36c82-default-rtdb.firebaseio.com/");
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly_register);
+
+        final EditText Name = findViewById(R.id.fullname);
+        final EditText Age = findViewById(R.id.age);
+        final EditText Email = findViewById(R.id.email);
+        final EditText Phone = findViewById(R.id.phone);
+        final EditText Location = findViewById(R.id.location);
+        final EditText Password = findViewById(R.id.password);
+        final EditText ConfirmPassword = findViewById(R.id.confirmpassword);
+
+        final Button RegisterButton = findViewById(R.id.RegisterBtn);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String fullname = Name.getText().toString();
+                final String age = Age.getText().toString();
+                final String email = Email.getText().toString();
+                final String phone = Phone.getText().toString();
+                final String location = Location.getText().toString();
+                final String password = Password.getText().toString();
+                final String confirmpassword = ConfirmPassword.getText().toString();
+
+                if (fullname.isEmpty() || age.isEmpty()||email.isEmpty()||phone.isEmpty()||location.isEmpty()||password.isEmpty()||confirmpassword.isEmpty()){
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter all the details";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else if(!password.equals(confirmpassword)){
+                    Context context = getApplicationContext();
+                    CharSequence text = "Passwords are not matching";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else{
+                    databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(phone)){
+                                Context context = getApplicationContext();
+                                CharSequence text = "Phone is already registered";
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                            }
+                            else{
+                                Context context = getApplicationContext();
+                                CharSequence text = "User Registered";
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                                finish();
+
+                                databaseReference.child("user").child(phone).child("name").setValue(fullname);
+                                databaseReference.child("user").child(phone).child("age").setValue(age);
+                                databaseReference.child("user").child(phone).child("email").setValue(email);
+                                databaseReference.child("user").child(phone).child("location").setValue(location);
+                                databaseReference.child("user").child(phone).child("password").setValue(password);
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+        });
 
         final TextView ElderlyLoginNow = findViewById(R.id.ElderlyLoginNow);
         ElderlyLoginNow.setOnClickListener(new View.OnClickListener() {
