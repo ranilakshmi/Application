b1bdae32e8f43688bcbadd209ba2711c1f71bc69
Aryan2601 <yoyo26new@gmail.com>
2022-11-20 13:59:58 +0530

    Added Switch View

---
+++ app/src/main/java/com/app3c/application/switchActivity.java
@@ -0,0 +1,12 @@
+package com.app3c.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class switchActivity extends AppCompatActivity {
+
+}
