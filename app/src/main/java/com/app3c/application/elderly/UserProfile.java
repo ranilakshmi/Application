c56294861e9102b561f6852260d7e58819af6ade
Pannaga01 <upsreya@gmail.com>
2023-02-05 17:00:55 +0530

    user_profile changes

--- app/src/main/java/com/app3c/application/elderly/UserProfile.java
+++ app/src/main/java/com/app3c/application/elderly/UserProfile.java
@@ -3,9 +3,12 @@ package com.app3c.application.elderly;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,33 +30,7 @@ public class UserProfile extends AppCompatActivity {
 
     //int[] userRatings = new int[3];
 
-
-    final EditText rating1 = findViewById(R.id.rating1);
-    final EditText rating2 = findViewById(R.id.rating2);
-    final EditText rating3 = findViewById(R.id.rating3);
-
-    final int r1 = rating1.getInputType();
-    final int r2 = rating1.getInputType();
-    final int r3 = rating1.getInputType();
-
-
-    content_based_recommendation ratings = new content_based_recommendation(r1,r2,r3);
-
-}
-
-class content_based_recommendation {
-
-    static int r1,r2,r3;
-
-    public content_based_recommendation(int r1, int r2, int r3) {
-        this.r1 = r1;
-        this.r2 = r2;
-        this.r3 = r3;
-    }
-
-    public static int[] movie_ratings = new int[]{r1,r2,r3};
-
-    public static String[] genres = new String[]{"a","b","c","d"};
+    public static String[] genres = new String[]{"children","education","environment","health"};
 
     public static int[][] generate_matrix(HashMap<Integer,String[]> movies, int start,int end){
 
@@ -103,15 +80,16 @@ class content_based_recommendation {
         return treeMap;
     }
 
-    public static void main(String[] args) {
+    public static String recommend(int[] movie_ratings) {
 
         HashMap<Integer, String[]> movie_genres = new HashMap<Integer, String[]>();
-        movie_genres.put(1,new String[]{"a","b","c"});
-        movie_genres.put(2,new String[]{"b","c","d"});
-        movie_genres.put(3,new String[]{"a"});
-        movie_genres.put(4,new String[]{"a","c","d"});
-        movie_genres.put(5,new String[]{"c"});
-        movie_genres.put(6,new String[]{"c","b"});
+        movie_genres.put(1, new String[]{"Children", "Education", "Environmemt"});
+        movie_genres.put(2, new String[]{"Children", "Education", "Health"});
+        movie_genres.put(3, new String[]{"Environment"});
+        movie_genres.put(4, new String[]{"Children", "Environment", "Health"});
+        movie_genres.put(5, new String[]{"Environment"});
+        movie_genres.put(6, new String[]{"Environment", "Education"});
+
 
         int total_no_of_movies = movie_genres.size();
 
@@ -126,6 +104,7 @@ class content_based_recommendation {
         float[] added_col = new float[4];
         int sum_added_col = 0;
 
+
         for (int j=0;j<4;j++){
             int sume = 0;
             for (int i=0;i<movie_ratings.length;i++){
@@ -135,36 +114,101 @@ class content_based_recommendation {
             added_col[j] = sume;
         }
 
-
-        for (int j=0;j<4;j++){
-            added_col[j] = added_col[j]/sum_added_col;
+        Map<Float,String> category_rating = new HashMap<>();
+        for (int j = 0; j < 4; j++) {
+            added_col[j] = added_col[j] / sum_added_col;
+            category_rating.put(added_col[j],genres[j] );
         }
-        /*
-         for (int i=0;i<added_col.length;i++){
-            System.out.println(added_col[i]);
-        }*/
+        category_rating = sortByKeys(category_rating);
+        String msg = "Recommended categories are ";
+        for (Map.Entry<Float, String> entry : category_rating.entrySet()) {
+            System.out.println("[" + entry.getKey() + ", " + (entry.getValue() + movie_ratings.length) + "]");
+            msg = msg + entry.getValue() +",";
+        }
+        Log.i("result", msg);
+
 
         int[][] candidate_movie_matrix = generate_matrix(movie_genres,movie_ratings.length,total_no_of_movies);
 
-        Map<Float,Integer> new_ratings = new HashMap<Float,Integer>();
+        Map<Float, Integer> new_ratings = new HashMap<Float, Integer>();
 
-        for (int i=0;i<candidate_movie_matrix.length;i++){
-            float weight=0;
-            for (int j=0;j<4;j++){
-                weight += candidate_movie_matrix[i][j]*added_col[j];
+        for (int i = 0; i < candidate_movie_matrix.length; i++) {
+            float weight = 0;
+            for (int j = 0; j < 4; j++) {
+                weight += candidate_movie_matrix[i][j] * added_col[j];
             }
-            new_ratings.put(weight,i);
+            new_ratings.put(weight, i);
         }
 
         new_ratings = sortByKeys(new_ratings);
 
         for (Map.Entry<Float, Integer>
-                entry : new_ratings.entrySet())
+                entry : new_ratings.entrySet()) {
             System.out.println(
                     "[" + entry.getKey()
-                            + ", " + (entry.getValue()+movie_ratings.length) + "]");
-
-
+                            + ", " + (entry.getValue() + movie_ratings.length) + "]");
+        }
+        return msg;
     }
+
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        RadioGroup radioGroup1;
+        RadioGroup radioGroup2;
+        RadioGroup radioGroup3;
+
+        Button submit;
+
+        submit = (Button)findViewById(R.id.submit_user_profile);
+
+        // Bind the components to their respective objects
+        // by assigning their IDs
+        // with the help of findViewById() method
+        radioGroup1 = (RadioGroup)findViewById(R.id.radioEvent1);
+        radioGroup2 = (RadioGroup)findViewById(R.id.radioEvent2);
+        radioGroup3 = (RadioGroup)findViewById(R.id.radioEvent3);
+
+        int selectedId1 = radioGroup1.getCheckedRadioButtonId();
+        int selectedId2 = radioGroup2.getCheckedRadioButtonId();
+        int selectedId3 = radioGroup3.getCheckedRadioButtonId();
+        RadioButton rating1 = (RadioButton)radioGroup1.findViewById(selectedId1);
+        RadioButton rating2 = (RadioButton)radioGroup2.findViewById(selectedId2);
+        RadioButton rating3 = (RadioButton)radioGroup3.findViewById(selectedId3);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+
+
+                final int r1 = rating1.getInputType();
+                final int r2 = rating2.getInputType();
+                final int r3 = rating3.getInputType();
+                int[] r={r1,r2,r3};
+                String msg  = recommend(r);
+                Context context = getApplicationContext();
+                int duration=Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context,msg,duration);
+                toast.show();
+                startActivity(new Intent(UserProfile.this,ElderlyLoginPage.class));
+            }
+        });
+
+
+
+
+
+    //content_based_recommendation ratings = new content_based_recommendation(r1,r2,r3);
+
+}
+
+
+
 }
 
