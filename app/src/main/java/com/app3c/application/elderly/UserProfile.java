a398bd27e1d842b96b4fbf5c20c12eedd974c23d
Arya R N <aryakunjamma@gmail.com>
2023-02-05 16:19:47 +0530

    Added code for recommending categories of volunteering events

--- app/src/main/java/com/app3c/application/elderly/UserProfile.java
+++ app/src/main/java/com/app3c/application/elderly/UserProfile.java
@@ -1,6 +1,7 @@
 package com.app3c.application.elderly;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app3c.application.R;
+import com.app3c.application.caretaker.CaretakerLoginPage;
+import com.app3c.application.caretaker.CaretakerRegistrationPage;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -20,7 +23,7 @@ import java.util.TreeMap;
 
 public class UserProfile extends AppCompatActivity {
 
-    public static String[] genres = new String[]{"a", "b", "c", "d"};
+    public static String[] genres = new String[]{"Children","Education","Environment","Health"};
 
     public static int[][] generate_matrix(HashMap<Integer, String[]> movies, int start, int end) {
 
@@ -68,15 +71,17 @@ public class UserProfile extends AppCompatActivity {
         return treeMap;
     }
 
-    void recommend(int [] movie_ratings) {
+
+
+    String recommend(int [] movie_ratings) {
 
         HashMap<Integer, String[]> movie_genres = new HashMap<Integer, String[]>();
-        movie_genres.put(1, new String[]{"a", "b", "c"});
-        movie_genres.put(2, new String[]{"b", "c", "d"});
-        movie_genres.put(3, new String[]{"a"});
-        movie_genres.put(4, new String[]{"a", "c", "d"});
-        movie_genres.put(5, new String[]{"c"});
-        movie_genres.put(6, new String[]{"c", "b"});
+        movie_genres.put(1, new String[]{"Children", "Education", "Environmemt"});
+        movie_genres.put(2, new String[]{"Children", "Education", "Health"});
+        movie_genres.put(3, new String[]{"Environment"});
+        movie_genres.put(4, new String[]{"Children", "Environment", "Health"});
+        movie_genres.put(5, new String[]{"Environment"});
+        movie_genres.put(6, new String[]{"Environment", "Education"});
 
         int total_no_of_movies = movie_genres.size();
 
@@ -100,10 +105,18 @@ public class UserProfile extends AppCompatActivity {
             added_col[j] = sume;
         }
 
-
+        Map<Float,String> category_rating = new HashMap<>();
         for (int j = 0; j < 4; j++) {
             added_col[j] = added_col[j] / sum_added_col;
+            category_rating.put(added_col[j],genres[j] );
         }
+        category_rating = sortByKeys(category_rating);
+        String msg = "Recommended categories are ";
+        for (Map.Entry<Float, String> entry : category_rating.entrySet()) {
+            System.out.println("[" + entry.getKey() + ", " + (entry.getValue() + movie_ratings.length) + "]");
+             msg = msg + entry.getValue() +",";
+        }
+        Log.i("result", msg);
         /*
          for (int i=0;i<added_col.length;i++){
             System.out.println(added_col[i]);
@@ -128,10 +141,11 @@ public class UserProfile extends AppCompatActivity {
             System.out.println(
                     "[" + entry.getKey()
                             + ", " + (entry.getValue() + movie_ratings.length) + "]");
-            String msg = "[" + entry.getKey()
-                    + ", " + (entry.getValue() + movie_ratings.length) + "]";
-            Log.i("result", msg);
+//            String msg = "[" + entry.getKey()
+//                    + ", " + (entry.getValue() + movie_ratings.length) + "]";
+//            Log.i("result", msg);
         }
+        return msg;
     }
 
     //int[] userRatings = new int[3];
@@ -152,7 +166,14 @@ public class UserProfile extends AppCompatActivity {
                                                 final int r2 = rating2.getInputType();
                                                 final int r3 = rating3.getInputType();
                                                 int[] r = {r1, r2, r3};
-                                                recommend(r);
+                                                String msg = recommend(r);
+                                                Context context = getApplicationContext();
+                                                //CharSequence text = "Please enter all the details";
+                                                int duration = Toast.LENGTH_LONG;
+
+                                                Toast toast = Toast.makeText(context,msg, duration);
+                                                toast.show();
+                                                startActivity(new Intent(UserProfile.this, ElderlyLoginPage.class));
                                             }
                                         });
 
